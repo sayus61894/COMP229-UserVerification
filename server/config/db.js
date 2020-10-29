c804f17a8562948b36291aa03bd3df3eda7a8815
Parth Shreyash Patel <parth.shreyash.patel@gmail.com>
2020-10-28 21:01:10 -0400

    Integrated with Mongo Atlas instead of local database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI" : "mongodb://localhost/contact"
+    //"URI" : "mongodb://localhost/contact"
+    "URI" :"mongodb+srv://sayus:gkV6a8LtE8AoiVGe@mongodbserver.7cksp.mongodb.net/contact?retryWrites=true&w=majority"
 }
